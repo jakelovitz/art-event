b814aed60ebdc7036312bf43d03467fb41a06d5e
Jason Gomez <jason.gomez@me.com>
2019-05-23 18:00:49 -0400

    Create form partial for User model.
    Signed-off-by: Jason Gomez <jason.gomez@me.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      login(@user)
+      redirect_to user_path(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.valid?
+      @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :username)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  resource :session, only: %i[new create destroy]
-  root to: redirect('/session/new')
+  resources :users, except: %i[index]
+  resource :login, only: %i[new create destroy]
+  root to: redirect('/login/new')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
