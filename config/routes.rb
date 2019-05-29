d0a78ba7543c34ad9d568b67a5528e74771667e8
Jason Gomez <jason.gomez@me.com>
2019-05-29 11:36:44 -0400

    Create basic navbar and controller action.
    Signed-off-by: Jason Gomez <jason.gomez@me.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, except: %i[index]
   resource :session, only: %i[new create destroy]
-  root to: redirect('/session/new')
+  # root to: redirect('/session/new')
 
   resources :user_events, only: %i[create destroy]
 
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   resources :events, only: %i[index, show]
   post '/events', to: 'events#query_api_with_location'
   match '/events' => redirect('/events/lookup'), via: :get
+
+  get '/', to: 'landing_page#home', as: 'home'
 end
